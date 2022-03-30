fb3b8dc0962a13419ea7519789f1cd621abd8ee5
Ezy93 <giordani001@gmail.com>
2022-03-30 15:50:07 +0200

    inizializzato e importato il componente card

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,6 +1,10 @@
 <template>
-    <nav>
-        
+    <nav class="navbar">
+        <div class="ms-3 py-2">
+            <a class="navbar-brand" href="#">
+                <img src="../assets/img/spotify-logo.png" alt="" width="30" height="30">
+            </a>
+        </div>
     </nav>
 </template>
 
@@ -11,5 +15,9 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+    nav{
+        background-color: #2e3a46 ;
+    }
 
 </style>
--- src/components/main.vue
+++ src/components/main.vue
@@ -1,17 +1,24 @@
 <template>
     <main>
-        
+        <div class="row">
+            <div class="col-8 offset-2">
+                <AlbumCard/>
+            </div>
+        </div>
     </main>
 </template>
 
 <script>
-
+import AlbumCard from './card.vue'
 
 export default {
     name:"IndexMain",
+    components: {
+        AlbumCard,
+    }
 }
 </script>
 
 <style lang="scss" scoped>
-
+    
 </style>
